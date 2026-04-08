6bf2dc897a2ecc74cec4f5a8cd3ebd669d17f8c3
SteveGamer13 <mateusmalikoski@gmail.com>
2026-04-07 21:14:55 -0300

    build site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "Artes-Conceituais-RE9"
+  base : "/Artes-Conceituais-Resident-Evil-Requiem/"
 })
